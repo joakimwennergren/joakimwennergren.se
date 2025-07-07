4bd4c83c173592e38f24fd4abf74a299f2c6a67f
Joakim Wennergren <joakim.wennergren@databeams.se>
2025-07-07 09:38:32 +0200

    add more tunes

--- src/outlets/index.jsx
+++ src/outlets/index.jsx
@@ -64,6 +64,10 @@ export default function Index() {
             src: "https://joakimwennergren.se/moongatan.wav"
         },
         {
+            title: "Hiup",
+            src: "https://joakimwennergren.se/hiup.wav"
+        },
+        {
             title: "Tourey kit",
             src: "https://joakimwennergren.se/tourey_kit.wav"
         },
@@ -72,12 +76,32 @@ export default function Index() {
             src: "https://joakimwennergren.se/bam.wav"
         },
         {
+            title: "Random",
+            src: "https://joakimwennergren.se/random.wav"
+        },
+        {
             title: "Poppy",
             src: "https://joakimwennergren.se/poppy.wav"
         },
         {
+            title: "Temp",
+            src: "https://joakimwennergren.se/temp.wav"
+        },
+        {
+            title: "Wonder",
+            src: "https://joakimwennergren.se/wonder.wav"
+        },
+        {
+            title: "Wtf",
+            src: "https://joakimwennergren.se/wtf.wav"
+        },
+        {
             title: "Technobeat",
             src: "https://joakimwennergren.se/technobeat.wav"
+        },
+        {
+            title: "Sad tune",
+            src: "https://joakimwennergren.se/sad_tune.wav"
         },
         {
             title: "Rich on water",
@@ -217,13 +241,12 @@ export default function Index() {
                             />
                         </Box>
                     ))}
+                    <Pagination count={totalMusicPages} page={currentMusicPage} onChange={handleMusicChange} />
                 </Box>
 
             )
         }
     }
-
-
 
     return (
         <>
